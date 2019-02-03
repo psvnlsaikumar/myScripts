5fa1e391d3e0b463302036f433c6effd09dd4306
Sai Kumar <saiyah007@localhost.localdomain>
2019-02-03 23:12:11 +0530

    LoopInspector

---
+++ LoopInspector.java
@@ -0,0 +1,9 @@
+/*  https://www.codewars.com/kata/52a89c2ea8ddc5547a000863/train/java */
+
+import java.io.*;
+import java.util.Scanner;
+class LoopInspector{
+  public static void main(String[] args){
+
+  }
+}
