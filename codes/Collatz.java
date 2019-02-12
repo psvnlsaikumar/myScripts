1c4107a652941e7eb9b34824fead38bd26a7f65d
Sai Kumar <saiyah0007@gmail.com>
2019-02-12 10:30:25 +0530

    Added count which gives the total number of steps taken by the logic

--- codes/Collatz.java
+++ codes/Collatz.java
@@ -12,7 +12,7 @@ public class Collatz{
 
 	public static String collatz(int n) {
 		String path = String.valueOf(n);
-		int count = 1;
+		int count = 0;
 		 while(!(n == 1)){	
 		    if(n % 2 == 0){
 		    	n = (n/2);
@@ -20,7 +20,9 @@ public class Collatz{
 		    	n = (3 * n) + 1;
 		    }
 		    	path += "->" + n;
+		    	count++;
 		  }
+		  System.out.println(count +" Steps have occured");
 		  return path;
 		}
 		
